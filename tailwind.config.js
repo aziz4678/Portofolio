bbf10ebd06ac95b3726b5bc56f76cd865127aad6
aziz4678 <muhamadaziz564@gmail.com>
2025-03-21 09:25:12 +0700

    tailwind, font

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        jetbrains: ['"JetBrains Mono"', 'monospace'],
+      },
+    },
+  },
+  plugins: [],
+}
