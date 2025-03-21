d8ca8e57c285aa5ba9853a275ea8991cec0925f4
aziz4678 <muhamadaziz564@gmail.com>
2025-03-21 15:58:27 +0700

    photo

--- src/components/Photo.jsx
+++ src/components/Photo.jsx
@@ -1,30 +1,48 @@
-import { useEffect, useState } from 'react';
-import photo from '../assets/photo.png'; 
+// eslint-disable-next-line no-unused-vars
+import { motion } from "framer-motion"
+import photo from '../assets/photo.jpg'
 
 const Photo = () => {
-  const [isVisible, setIsVisible] = useState(false);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setIsVisible(true);
-    }, 100); 
-
-    return () => clearTimeout(timer);
-  }, []);
-
   return (
-    <div className="w-full h-full relative">
-      <div
-        className={`xl:w-[500px] xl:h-[300px] transition-opacity duration-500 ${isVisible ? 'opacity-100' : 'opacity-0'} mix-blend-lighten -mt-16`}
+    <div className="relative w-full h-full flex justify-center items-center">
+      <motion.svg
+        className="absolute"
+        width="100%"
+        height="100%"
+        viewBox="0 0 500 500"
       >
+        <motion.circle
+          cx="253"
+          cy="253"
+          r="240"
+          stroke="#00ff99"
+          strokeWidth="6"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          initial={{ strokeDasharray: "24 10 0 0" }}
+          animate={{
+            strokeDasharray: ["15 120 25 25", "16 25 92 72", "4 250 22 22"],
+            rotate: [0, 360]
+          }}
+          transition={{
+            duration: 20,
+            repeat: Infinity,
+            repeatType: "reverse",
+            ease: "easeInOut"
+          }}
+        />
+      </motion.svg>
+
+      <div className="flex-shrink-0 w-[400px] h-[400px] relative">
         <img
-          src={photo} 
+          src={photo}
           alt="Photo"
-          className="object-contain w-full h-full"
+          className="object-cover w-full h-full rounded-full"
+          style={{ objectPosition: 'top' }}
         />
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Photo;
+export default Photo
